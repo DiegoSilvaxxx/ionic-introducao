9ce85eeb70912f6bea108b504269825ed7d784cd
DiegoSilvaxxx <diegoxbdasilva@gmail.com>
2019-03-25 21:31:09 -0300

    ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,23 +23,22 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: 'HomePage' },
       { title: 'List', component: 'ListPage' },
-      { title: 'Teste', component: 'TesteIonicPage' }
+      { title: 'Teste', component: 'TesteIonicPage' },
+      { title: 'Contato', component: 'FormContatePage' }
     ];
 
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+     
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
+   
     this.nav.setRoot(page.component);
   }
 }
